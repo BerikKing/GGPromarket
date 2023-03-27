281401b8d572febe69e2d59076445d00b50e1fa6
Berik <berloo24@gmail.com>
2023-03-27 20:00:58 +0600

    js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,31 @@
-const sliderContainer = document.querySelector('.slider-container');
-const prevBtn = document.querySelector('.prev');
-const nextBtn = document.querySelector('.next');
+const sliderContainer = document.querySelector(".slider-container");
+const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");
 let counter = 0;
 const size = sliderContainer.children[0].clientWidth;
 
-nextBtn.addEventListener('click', () => {
+nextBtn.addEventListener("click", () => {
   if (counter < sliderContainer.children.length - 1) {
     counter++;
-    sliderContainer.style.transform = `translateX(-${size * counter}px)`;
+    sliderContainer.style.transform = `translateX(-${size}px)`;
   }
 });
 
-prevBtn.addEventListener('click', () => {
+prevBtn.addEventListener("click", () => {
   if (counter > 0) {
     counter--;
-    sliderContainer.style.transform = `translateX(-${size * counter}px)`;
+    sliderContainer.style.transform = `translateX(-${size}px)`;
   }
 });
+
+
+
+
+let loader = document.querySelector('.loader')
+
+// window.addEventListener('load',function (){
+//   loader.style.display = "none";
+// })
+setTimeout(function(){
+  loader.style.display = "none"
+},3000)
