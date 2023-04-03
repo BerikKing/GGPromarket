65bd28f9b80dbb763c3a669a3125912f5bf3c08a
Berik <berloo24@gmail.com>
2023-04-03 19:07:02 +0600

    loader

--- js/script.js
+++ js/script.js
@@ -5,57 +5,54 @@ let counter = 0;
 const size = sliderContainer.children[0].clientWidth;
 
 nextBtn.addEventListener("click", () => {
-  if (counter < sliderContainer.children.length - 1) {
-    counter++;
-    sliderContainer.style.transform = `translateX(-${size}px)`;
-  }
+    if (counter < sliderContainer.children.length - 1) {
+        counter++;
+        sliderContainer.style.transform = `translateX(-${size}px)`;
+    }
 });
 
 prevBtn.addEventListener("click", () => {
-  if (counter > 0) {
-    counter--;
-    sliderContainer.style.transform = `translateX(-${size}px)`;
-  }
+    if (counter > 0) {
+        counter--;
+        sliderContainer.style.transform = `translateX(-${size}px)`;
+    }
 });
 
-let loader = document.querySelector('.loader');
+let loader = document.querySelector(".loader");
 
+window.addEventListener("load", function () {
+    loader.style.display = "none";
+});
 
-window.addEventListener('load', function () {
-    loader.style.display = 'none';
-})
-
-setTimeout(function () {
-    loader.style.display = 'none';
-}, 3000)
+// setTimeout(function () {
+//     loader.style.display = 'none';
+// }, 3000)
 
 let hits = $(".hits");
 let pro = $(".promotions");
 let article = $(".article_wrapper");
 
 pro.slick({
-  infinite: true,
-  slidesToShow: 4,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 1000,
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
 });
 hits.slick({
-  infinite: true,
-  slidesToShow: 4,
-  slidesToScroll: 1,
-  autoplay: false,
-  autoplaySpeed: 1000,
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 1000,
 });
 article.slick({
-  infinite: true,
-  slidesToShow: 4,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 2000,
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
 });
-
-
 
 // $('.hits').slick({
 //   infinite: true,
