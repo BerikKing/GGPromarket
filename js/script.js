3d64d16377dff41ceeccefb4ef350eb913ada033
Berik <berloo24@gmail.com>
2023-04-12 18:44:26 +0600

    addJson

--- js/script.js
+++ js/script.js
@@ -180,15 +180,18 @@ let form = document.querySelector(".form");
 form.addEventListener("submit", async function (event) {
     event.preventDefault(); //refresh(перезгрузка алып тастайды)
 
-    let formData = new formData(form);
+    let formData = new FormData(form);
 
     let infoUser = Object.fromEntries(formData);
-    await fetch("http://localhost:3000/user"),
-        {
-            method: "POST",
-            body: JSON.stringify(infoUser),
-            headers: {
-                "Content-Type": "application/json",
-            },
-        };
+    await fetch("http://localhost:3000/user", {
+        method: "POST",
+        body: JSON.stringify(infoUser),
+        headers: {
+            "Content-Type": "application/json",
+        },
+    }).then((res) => {
+        if (res.status === 201) {
+            localStorage.setItem("userinifo", JSON.stringify(infoUser));
+        }
+    });
 });
